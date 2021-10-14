bc2b79f105a9ca09a7881dbec6e80b5f27048b21
jimmyhsiao7 <42952630+jimmyhsiao7@users.noreply.github.com>
2021-10-14 19:54:09 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-let j = 0
+let ret = false
 let i = 0
+let j = 0
 let c = 1
 basic.forever(function () {
+    j = 0
     i = 0
     for (let index = 0; index < 5; index++) {
         for (let index = 0; index < 5; index++) {
@@ -18,4 +20,26 @@ basic.forever(function () {
         i += 1
     }
     c += 1
+    i = 0
+    ret = false
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(j, i)
+            if (ret) {
+                j += -1
+            } else {
+                j += 1
+            }
+            basic.pause(100)
+            basic.clearScreen()
+        }
+        if (j >= 5) {
+            ret = true
+            j += -1
+        } else {
+            ret = false
+            j += 1
+        }
+        i += 1
+    }
 })
